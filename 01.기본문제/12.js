dfe0149a844befc15626c3d939d38e3a60dcfd9a
Kay <duddnr654@naver.com>
2023-03-15 13:29:56 +0900

    Log: 기본문제 - 12번

---
+++ 01.기본문제/12.js
@@ -0,0 +1,41 @@
+// 모든 문자를 대문자로 만드는 함수
+const solution1 = (words) => {
+  let result = "";
+  for (let word of words) {
+    result += word.toUpperCase();
+  }
+
+  return result;
+};
+
+const solution2 = (words) => {
+  let result = "";
+  for (let word of words) {
+    if (word === word.toLowerCase()) {
+      result += word.toUpperCase();
+    } else {
+      result += word;
+    }
+  }
+
+  return result;
+};
+
+const solution3 = (words) => {
+  let result = "";
+  for (let word of words) {
+    const num = word.charCodeAt();
+
+    if (num >= 97 && num <= 122) {
+      result += String.fromCharCode(num - 32);
+    } else {
+      result += word;
+    }
+  }
+
+  return result;
+};
+
+const words = "ItisTimeToStudy";
+const result3 = solution3(words);
+console.log(result3);
