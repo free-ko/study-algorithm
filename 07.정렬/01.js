7c80ac2245ab835913f3fdbe7166f69842fc08df
Kay <duddnr654@naver.com>
2023-05-10 16:39:43 +0900

    Log: 정렬 - 1번(선택정렬)

--- 07.정렬/01.js
+++ 07.정렬/01.js
@@ -12,6 +12,23 @@ const solution1 = (arr) => {
   return result;
 };
 
+const solution2 = (arr) => {
+  const result = arr;
+  for (let i = 0; i < result.length - 1; i++) {
+    let idx = i;
+
+    for (let j = i + 1; j < result.length; j++) {
+      if (result[j] < result[idx]) {
+        idx = j;
+      }
+    }
+
+    [result[i], result[idx]] = [result[idx], result[i]];
+  }
+
+  return result;
+};
+
 const arr = [13, 5, 11, 7, 23, 15];
-const result1 = solution1(arr);
+const result1 = solution2(arr);
 console.log(result1);
