1dd4249b0c3375d03119d652b5848faec5eb0ef6
Kay <duddnr654@naver.com>
2023-05-13 14:19:39 +0900

    Log: 정렬 - 4번(복습)

--- 07.정렬/04.js
+++ 07.정렬/04.js
@@ -19,6 +19,25 @@ const solution1 = (arr) => {
   return result;
 };
 
-const arr = [11, 7, 5, 6, 10, 9];
-const result1 = solution1(arr);
+const solution2 = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let j;
+    let tmp = arr[i];
+
+    for (j = i - 1; 0 <= j; j--) {
+      if (tmp < arr[j]) {
+        arr[j + 1] = arr[j];
+      } else {
+        break;
+      }
+    }
+
+    arr[j + 1] = tmp;
+  }
+
+  return arr;
+};
+
+const arr = [7, 11, 5, 6, 10, 9];
+const result1 = solution2(arr);
 console.log(result1);
