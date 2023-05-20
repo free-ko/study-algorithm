163566554a3419036dd409403d17274aa2dec391
Kay <duddnr654@naver.com>
2023-05-20 11:32:40 +0900

    Log: 정렬 10번(복습)

--- 07.정렬/10.js
+++ 07.정렬/10.js
@@ -28,6 +28,36 @@ const solution1 = (target, arr) => {
   return result;
 };
 
+const solution2 = (target, arr) => {
+  let result = 0;
+  let lt = 0;
+  let rt = arr.length - 1;
+  const sortedArr = [...arr].sort((a, b) => a - b);
+
+  while (lt <= rt) {
+    let mid = Math.floor((lt + rt) / 2);
+
+    if (sortedArr[mid] === target) {
+      result = mid + 1;
+      break;
+    }
+
+    if (target < sortedArr[mid]) {
+      rt = mid - 1;
+      mid = Math.floor((lt + rt) / 2);
+      continue;
+    }
+
+    if (sortedArr[mid] < target) {
+      lt = mid + 1;
+      mid = Math.floor((lt + rt) / 2);
+      continue;
+    }
+  }
+
+  return result;
+};
+
 const test = [23, 87, 65, 12, 57, 32, 99, 81];
-// const result1 = solution1(32, test);
-// console.log(result1);
+const result1 = solution1(32, test);
+console.log(result1);
