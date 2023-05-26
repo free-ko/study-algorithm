a815baee2c9b4584a57696d683d547ae7110e95b
kay <duddnr654@naver.com>
2023-05-26 21:30:26 +0900

    Log: 재귀함수와 완전탐색(1번)

---
+++ 08.재귀함수와 완전탐색/01.js
@@ -0,0 +1,14 @@
+// Stack 자료 구조에 의해 재귀함수가 실행
+const solution = (n) => {
+  const DFS = (L) => {
+    if (L === 0) return;
+
+    DFS(L - 1);
+    console.log(L);
+  };
+
+  DFS(n);
+};
+
+const result = solution(3);
+console.log(result);
