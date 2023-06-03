10f965d1c424141814aafc9f9d458d351e4b2648
kay <duddnr654@naver.com>
2023-06-03 19:11:18 +0900

    Log: 재귀함수와 완전탐색(5번)

---
+++ 08.재귀함수와 완전탐색/05.js
@@ -0,0 +1,35 @@
+/**
+ * 합이 같은 부분집합(DFS)
+ *  ㄴ N개의 원소로 구성된 자연수 집합이 주어지면, 이 집합을 두개의 부분집합으로 나누었을 때
+ *  ㄴ 두 부분 집합의 원소의 합이 서로 같은 경우 존재하면 YES
+ *  ㄴ 그렇지 않으면, NO
+ */
+
+const solution = (arr) => {
+  let result = 'NO';
+  let flag = 0;
+  let total = arr.reduce((a, b) => a + b, 0);
+  let n = arr.length;
+
+  const DFS = (L, sum) => {
+    if (flag) return;
+
+    if (L === n) {
+      if (total - sum === sum) {
+        result = 'YES';
+        flag = 1;
+      }
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  };
+
+  DFS(0, 0);
+
+  return result;
+};
+
+const test = [1, 3, 5, 6, 7, 10];
+const result = solution(test);
+console.log(result);
