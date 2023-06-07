9449d4563d8717850d8146d9a403de8bf3308513
kay <duddnr654@naver.com>
2023-06-07 21:35:50 +0900

    Log: 재귀함수와 완전탐색(8번)

---
+++ 08.재귀함수와 완전탐색/08.js
@@ -0,0 +1,28 @@
+/**
+ * 중복 순열 구하기
+ *  ㄴ 1부터 N까지 번호가 적힌 구슬이 있습니다.
+ *  ㄴ 중복을 허락하여 M번을 뽑아 일렬로 나열하는 방법
+ */
+
+const solution = (n, m) => {
+  let result = [];
+  let tmp = Array.from({ length: m }, () => 0);
+
+  const DFS = (L) => {
+    if (L === m) {
+      result.push([...tmp]);
+    } else {
+      for (let i = 1; i <= n; i++) {
+        tmp[L] = i;
+        DFS(L + 1);
+      }
+    }
+  };
+
+  DFS(0);
+
+  return result;
+};
+
+const result = solution(3, 2);
+console.log(result);
