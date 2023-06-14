0b070051e8726b3b12c9a1a98bea4f920127b8ff
kay <duddnr654@naver.com>
2023-06-14 21:09:51 +0900

    Log: 재귀함수와 완전탐색(11번)

---
+++ 08.재귀함수와 완전탐색/11.js
@@ -0,0 +1,21 @@
+/**
+ * 팩토리얼
+ *  ㄴ 5! = 5 x 4 x 3 x 2 x 1
+ */
+
+const solution = (n) => {
+  let result;
+
+  const DFS = (L) => {
+    if (L === 1) return 1;
+
+    return L * DFS(L - 1);
+  };
+
+  result = DFS(n);
+
+  return result;
+};
+
+const result = solution(5);
+console.log(result);
