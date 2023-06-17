2a0f85298dece1af95b50e82618a8e3f0742fe86
kay <duddnr654@naver.com>
2023-06-17 13:07:33 +0900

    Log: 재귀함수와 완전탐색(12번)

---
+++ 08.재귀함수와 완전탐색/12.js
@@ -0,0 +1,22 @@
+/**
+ * 조합의 경우 수(메모이제이션)
+ */
+
+const solution = (n, r) => {
+  let result;
+  const dy = Array.from(Array(35), () => Array(55).fill(0));
+
+  const DFS = (n, r) => {
+    if (dy[n][r] > 0) return dy[n][r];
+
+    if (n === r || r === 0) return 1;
+
+    return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+  };
+
+  result = DFS(n, r);
+  return result;
+};
+
+const result = solution(33, 19);
+console.log(result);
