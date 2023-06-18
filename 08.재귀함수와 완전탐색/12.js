a82d7a2fda487d7fc2566b29ce6f958231fcf446
kay <duddnr654@naver.com>
2023-06-18 15:59:40 +0900

    Log: 재귀함수와 완전탐색(12번 복습)

--- 08.재귀함수와 완전탐색/12.js
+++ 08.재귀함수와 완전탐색/12.js
@@ -18,5 +18,21 @@ const solution = (n, r) => {
   return result;
 };
 
-const result = solution(33, 19);
+const practice = (n, r) => {
+  let result;
+  const dy = Array.from(Array(35), () => Array(55).fill(0));
+
+  const DFS = (n, r) => {
+    if (0 < dy[n][r]) return dy[n][r];
+
+    if (n === r || r === 0) return 1;
+    return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+  };
+
+  result = DFS(n, r);
+
+  return result;
+};
+
+const result = practice(33, 19);
 console.log(result);
