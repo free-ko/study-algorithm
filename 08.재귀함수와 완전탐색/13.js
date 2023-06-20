b9c7dfe8869a38ae3bf51e04136508446d21c845
kay <duddnr654@naver.com>
2023-06-20 22:50:57 +0900

    Log: 재귀함수와 완전탐색(13번 복습)

--- 08.재귀함수와 완전탐색/13.js
+++ 08.재귀함수와 완전탐색/13.js
@@ -44,5 +44,48 @@ const solution = (n, f) => {
   return result;
 };
 
-const result = solution(4, 16);
+const practice = (n, f) => {
+  let result;
+  let flag = 0;
+  let dy = Array.from(Array(11), () => Array(11).fill(0));
+  let ch = Array.from({ length: n + 1 }, () => 0);
+  let p = Array.from({ length: n }, () => 0);
+  let b = Array.from({ length: n }, () => 0);
+
+  const combi = (n, r) => {
+    if (0 < dy[n][r]) return dy[n][r];
+
+    if (n === r || r === 0) return 1;
+
+    return (dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r));
+  };
+
+  const DFS = (L, sum) => {
+    if (flag) return;
+
+    if (L === n && sum === f) {
+      result = p.slice();
+      flag = 1;
+    } else {
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === 0) {
+          ch[i] = 1;
+          p[L] = i;
+          DFS(L + 1, sum + b[L] * p[L]);
+          ch[i] = 0;
+        }
+      }
+    }
+  };
+
+  for (let i = 0; i < n; i++) {
+    b[i] = combi(n - 1, i);
+  }
+
+  DFS(0, 0);
+
+  return result;
+};
+
+const result = practice(4, 16);
 console.log(result);
